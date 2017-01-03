09f7089f2ad0b00679f878a20c5bed0700485690
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-01-02 20:49:26 -0500

    small additions

--- freqstuff.py
+++ freqstuff.py
@@ -6,11 +6,23 @@ MOVE_SIZE = 512
 
 notes = [(y[0],float(y[1])) for y in [x.split("\t") for x in open("notefreqs.txt").read().split("\n")]]
 
+# ((bin_id) * freq / 2) / (N/2) where freq is sample freq
 
-# returns magnitudes of each frequency
+## returns the closest match note to a frequency
+def findnote(freq):
+    bestnote = notes[0][0]
+    bestdist = abs(notes[0][1]-freq)
+    for i in range(1,len(notes)):
+        if(abs(notes[i][1]-freq)<bestdist):
+            bestdist = abs(notes[i][1]-freq)
+            bestnote = notes[i][0]
+    return bestnote
+
+## returns magnitudes of each frequency
 def freqbreakdown(snd):
     f = np.fft.fft(snd)
     return [abs(x) for x in f[0:len(f)//2]]
+
 
 rate, sound = wavfile.read("song.wav")
 
